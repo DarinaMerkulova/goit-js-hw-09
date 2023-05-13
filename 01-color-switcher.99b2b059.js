c8fcf27ce7ba1c8c80b1e778a60d6de47649193c
DarinaMerkulova <119629811+DarinaMerkulova@users.noreply.github.com>
2023-05-13 20:20:18 +0000

    Deploying to gh-pages from @ DarinaMerkulova/goit-js-hw-09@fab8d73fed9103d4446047c90e958cfc1d78ed06 🚀

--- 01-color-switcher.99b2b059.js
+++
@@ -1,2 +0,0 @@
-const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]"),body:document.querySelector("body")};t.stopBtn.disabled=!0;let e=null;t.startBtn.addEventListener("click",(()=>{e=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.stopBtn.disabled=!1,t.startBtn.disabled=!0})),t.stopBtn.addEventListener("click",(()=>{t.stopBtn.disabled=!0,t.startBtn.disabled=!1,clearInterval(e)}));
-//# sourceMappingURL=01-color-switcher.99b2b059.js.map
